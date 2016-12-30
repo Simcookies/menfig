ac20038fd7c7fa77bc6ca468229421af4e74beb3
Song <mssongan840136@outlook.com>
2016-12-30 19:55:20 +0900

    Temp commit

---
+++ package/gui.py
@@ -0,0 +1,33 @@
+import Tkinter as tk
+import pygubu
+from tkFileDialog import askopenfilename
+
+class Application:
+    def __init__(self, master):
+        self.master = master
+        self.builder = builder = pygubu.Builder()
+        builder.add_from_file('gui.ui')
+        self.mainwindow = builder.get_object('mainWindow', master)
+        builder.connect_callbacks(self)
+
+    def explore_file_s11(self):
+        self.explore_file(self, 'strDataFilePath_S11')
+
+    def explore_file_s21(self):
+        self.explore_file(self, 'strDataFilePath_S21')
+
+    @staticmethod
+    def explore_file(master, objId):
+        # Open file from File Explorer and keep file path
+        filename = askopenfilename(filetypes=[("CSV Data files","*.csv")])
+
+        # Replace value of Entey
+        objDataFile = master.builder.get_object(objId)
+        objDataFile.delete(0, 'end')
+        objDataFile.insert(0, filename)
+
+def run():
+    root = tk.Tk()
+    app = Application(root)
+
+    root.mainloop()
--- script.py
+++ script.py
@@ -2,7 +2,9 @@
 
 import package.dataprocess as dp
 import matplotlib.pyplot as plt
+import package.gui as gui
 
+gui.run()
 dp.get_csv_data("/Users/songan/workspace/python/analysis/data/S11_1.csv")
 plt.plot(dp.data_x_axis(), dp.data_y_axis())
 
@@ -12,5 +14,5 @@ plt.plot(dp.data_x_axis(), dp.data_y_axis(), color="red", linestyle="--")
 dp.get_csv_data("/Users/songan/workspace/python/analysis/data/S22_1.csv")
 plt.plot(dp.data_x_axis(), dp.data_y_axis(), color="red", linestyle="--")
 
-plt.show()
+# plt.show()
 
