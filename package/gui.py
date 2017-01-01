ba8b7f926244f53860968277ce3f1e0cc362e0fe
Song <mssongan840136@outlook.com>
2017-01-01 18:37:53 +0900

    Add fixtures of line color and style, modified GUI

--- package/gui.py
+++ package/gui.py
@@ -4,7 +4,13 @@ from tkinter import messagebox
 
 import pygubu
 import package.dataprocess as dp
+import matplotlib
+
+# avoid from crashing after import tkinter (on Mac)
+matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
+
+line_style_dic = { 'Dashed': '--', 'Dotted': ':', 'Solid': '-', 'Dash-Dotted': '-.' }
 
 class Application:
     def __init__(self, master):
@@ -15,19 +21,27 @@ class Application:
         builder.connect_callbacks(self)
 
     def draw_figure(self):
+        # Get file name, line color and line style
         filename = self.builder.get_object('strDataFilePath').get()
+        objSelectColor = self.builder.get_object('selectColor')
+        objSelectLine = self.builder.get_object('selectLineStyle')
+
+        line_color = objSelectColor["text"].lower()
+        line_style = line_style_dic[objSelectLine["text"]]
+        
         if filename == '':
             messagebox.showinfo('From callback', 'File name can not be blank.')
             return None
         try:
+            # Read from data file and draw figure
             dp.get_csv_data(filename)
-            self.plot_data = plt.plot(dp.data_x_axis(), dp.data_y_axis())
+            self.plot_data = plt.plot(dp.data_x_axis(), dp.data_y_axis(),
+                                      color=line_color, linestyle=line_style)
             plt.show()
         except:
             messagebox.showinfo('From callback', 'File can not be found.')
 
     def quit_app(self):
-        plt.close('all')
         self.master.quit()
 
     def explore_file(self):
@@ -38,6 +52,14 @@ class Application:
         objDataFile = self.builder.get_object('strDataFilePath')
         objDataFile.delete(0, 'end')
         objDataFile.insert(0, filename)
+
+    def on_colormenu_clicked(self, itemId):
+        objSelectColor = self.builder.get_object('selectColor')
+        objSelectColor["text"] = itemId[5:]
+
+    def on_linemenu_clicked(self, itemId):
+        objSelectLine = self.builder.get_object('selectLineStyle')
+        objSelectLine["text"] = itemId[4:]
 
 def run():
     root = tk.Tk()
--- script.py
+++ script.py
@@ -3,14 +3,3 @@
 import package.gui as gui
 
 gui.run()
-# dp.get_csv_data("/Users/songan/workspace/python/analysis/data/S11_1.csv")
-# plt.plot(dp.data_x_axis(), dp.data_y_axis())
-
-# dp.get_csv_data("/Users/songan/workspace/python/analysis/data/S21_1.csv")
-# plt.plot(dp.data_x_axis(), dp.data_y_axis(), color="red", linestyle="--")
-
-# dp.get_csv_data("/Users/songan/workspace/python/analysis/data/S22_1.csv")
-# plt.plot(dp.data_x_axis(), dp.data_y_axis(), color="red", linestyle="--")
-
-# plt.show()
-
